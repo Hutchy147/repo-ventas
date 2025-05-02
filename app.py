59642f2041093de4e319ec06f238ea753b3b284a
mateo <mateogomez3521@gmail.com>
2025-05-02 08:53:21 -0300

    añado products y variantes

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from config import Config
+from extensions import db
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+
+    # Importar solo rutas de productos (en esta rama)
+    from routes.product_routes import product_bp
+    app.register_blueprint(product_bp, url_prefix="/api/products")
+
+    return app
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True)
