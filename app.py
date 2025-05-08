01ec7708190cfd30741585327004117a4c09cfb4
Muñoz Tomás <tomas.gem464@gmail.com>
2025-05-08 17:18:01 -0300

    class and proven method

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from config import Config
+from database import db
+from models.category import Category
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    with app.app_context():
+        from models.category import Category 
+        db.create_all()  # Esto creará todas las tablas de tus modelos importados
+
+    # Importar solo rutas de productos (en esta rama)
+    from routes.categories import category_bp
+    app.register_blueprint(category_bp, url_prefix="/api/categories")
+
+    return app
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True)
--- models/category.py
+++ models/category.py
@@ -1,13 +1,13 @@
-from app import db
+from database import db
 
-class category(db.model):
-    __tablename__ = "category"
+class Category(db.Model):
+    __tablename__ = "categories"
 
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(100), nullable = False)
     description = db.Column(db.String(250), nullable = True)
 
-    products = db.relationship("Produc", beckref="Category", lazy=True)
+    #products = db.relationship("Produc", beckref="Category", lazy=True)
 
     def to_dict(self):
         return {
