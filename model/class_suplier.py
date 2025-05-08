044db0b728ea926f436f2efaa01b21ed18c8cdc8
Leandro Briceño <leagustin9@gmail.com>
2025-05-08 00:13:08 -0300

    Uploading supplier file
    subiendo archivos del proveedor

---
+++ model/class_suplier.py
@@ -0,0 +1,29 @@
+#Entidad Proveedor
+from Database import db
+
+class Suplier(db.Model):
+    __tablename__="suplier"
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(100),nullable=True)
+    adress=db.Column(db.String(100),nullable=True)
+    phone=db.Column(db.Integer(50),unique=True,nullable=True)
+    website=db.Column(db.String(500),unique=True,nullable=True)
+    rut=db.Column(db.Integer,unique=True,nullable=True)
+
+    def __init__(self,name,adress,phone,website,rut):
+        self.name=name
+        self.adress=adress
+        self.phone=phone
+        self.website=website
+        self.rut=rut
+    
+
+    def serialize(self):
+        return {
+            "name":self.name,
+            "adress":self.adress,
+            "phone":self.phone,
+            "website":self.website,
+            "rut":self.rut
+        }
+        
---
+++ route/suplier_route.py
@@ -0,0 +1,50 @@
+from flask import Blueprint,request,jsonify
+from Database import db
+from model.class_suplier import Suplier
+
+suplier=Blueprint("suplier",__name__)
+
+@suplier.route("/api/get_suplier",methods=["GET"])
+
+def get_suplier():
+    supliers=Suplier.query.all()#Obtengo los datos de los proveedores en la base de datos
+    
+    return jsonify([suplier.serialize() for suplier in supliers])
+
+@suplier.route("/api/post_suplier",methods=["POST"])
+
+def post_suplier():
+    data=request.get_json()
+    
+    if not data or not all(key in data for key in ["name","adress","website","rut"]):
+        return jsonify({"Error":"Datos no encontrados"}),400
+    
+    if Suplier.query.filter_by(rut=data["rut"]).first():#verifico si el rut del proveedor ya existe
+        return jsonify({"Error": "Rut ya registrado"}),400
+    
+    try:
+
+        new_suplier=Suplier(data["name"],data["adress"],data["website"],data["rut"])
+        db.session.add(new_suplier)
+        db.session.commit()
+    
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"Error":str(e)}),500
+    
+@suplier.route("/api/delete_suplier",methods=["DELETE"])
+
+def delete_suplier(id):
+    suplier=Suplier.query.get(id)
+    if not suplier:
+        return jsonify({"Mensaje","No se encuentra proveedor"}),404
+    
+    try:
+        db.session.delete(suplier)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"Error":str(e)}),500
+
+
+
