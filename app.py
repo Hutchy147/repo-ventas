d2a5124aeaf510c52ee4af86e09abd89c68e7f1a
Muñoz Tomás <tomas.gem464@gmail.com>
2025-05-09 13:59:36 -0300

    arreglos

--- app.py
+++ app.py
@@ -1,27 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
 from database import db
-from routes.suppeliers import suppelier_bp
-from routes.categories import categories_bp
-from routes.products import product_bp
 from routes.clients import client_bp
+from routes.products import product_bp
+from routes.suppeliers import suppelier_bp
+from routes.categories import category_bp
 
 def create_app():
     app = Flask(__name__)
-
-    # Configuración de la base de datos
     app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:root@localhost/ventas_db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
 
-    # Crear las tablas en la base de datos (si no existen)
     with app.app_context():
         db.create_all()
 
-    # Registrar los blueprints para las rutas
-    app.register_blueprint(suppelier_bp, url_prefix="/api/suppeliers")
-    app.register_blueprint(categories_bp, url_prefix="/api/categories")
-    app.register_blueprint(product_bp, url_prefix="/api/products")
+    # Blueprints
     app.register_blueprint(client_bp, url_prefix="/api/clients")
+    app.register_blueprint(product_bp, url_prefix="/api/products")
+    app.register_blueprint(suppelier_bp, url_prefix="/api/suppeliers")
+    app.register_blueprint(category_bp, url_prefix="/api/categories")
+
+    # Página principal
+    @app.route("/")
+    def index():
+        return render_template("index.html")
 
     return app
 
