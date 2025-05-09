e084288edd72b7cb7a2da839f00e4d6da3250bfc
mateo <mateogomez3521@gmail.com>
2025-05-08 21:34:43 -0300

    terminado product

--- models/product.py
+++ models/product.py
@@ -7,12 +7,14 @@ class Product(db.Model):
     nombre = db.Column(db.String(100), nullable=False)
     marca = db.Column(db.String(100), nullable=False)
     stock = db.Column(db.Integer, default=0)
+    precio_actual = db.Column(db.Float, nullable=False, default=0.0)
 
     def to_dict(self):
         return {
             "id": self.id,
             "nombre": self.nombre,
             "marca": self.marca,
-            "stock": self.stock
-        }
+            "stock": self.stock,
+            "precio_actual": self.precio_actual
+            }
 
