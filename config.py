7dc7355f90cb1c8a41fb6deddfade2386e9e7a34
Muñoz Tomás <tomas.gem464@gmail.com>
2025-05-09 17:59:55 -0300

    General create

--- config.py
+++ config.py
@@ -1,3 +1,11 @@
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost/ventas_db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+user = os.getenv("USER")
+password = os.getenv("PASSWORD")
+host = os.getenv("HOST")
+database = os.getenv("DB")
+
+DATABASE_CONNECTION = f"mysql+pymysql://{user}:{password}@{host}/{database}"
